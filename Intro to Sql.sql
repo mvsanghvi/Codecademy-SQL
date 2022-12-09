22137255972731479ce1e07056adeb8f493c7299
mvsanghvi <mevsanghvi@gmail.com>
2022-12-08 18:49:55 -0600

    Initial Commit

---
+++ Intro to Sql.sql
@@ -0,0 +1,15 @@
+-- SQL, Structured Query Language, is a programming language designed to manage data stored in relational databases. 
+-- SQL operates through simple, declarative statements. 
+-- This keeps data accurate and secure, and helps maintain the integrity of databases, regardless of size.
+-- The SQL language is widely used today across web frameworks and database applications. 
+-- Knowing SQL gives you the freedom to explore your data, and the power to make better decisions. 
+-- By learning SQL, you will also learn concepts that apply to nearly every data storage system.
+-- The statements covered in this course use SQLite Relational Database Management System (RDBMS). 
+-- You can also access a glossary of all the SQL commands taught in this course.
+
+-- 1. Let’s begin by entering a SQL command. In the code editor, type:
+
+SELECT * FROM celebs;
+
+-- You will run all of your SQL commands in this course by pressing the Run button at the bottom of the code editor.
+-- Press Run.
