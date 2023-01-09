f8a5274081e4dc8fd7ea2100301151ef4627e6e9
mvsanghvi <mevsanghvi@gmail.com>
2023-01-09 16:42:44 -0600

    Initial Commit

---
+++ Queries/2) Select.sql
@@ -0,0 +1,14 @@
+-- Previously, we learned that SELECT is used every time you want to query data from a database and * means all columns.
+
+-- Suppose we are only interested in two of the columns. We can select individual columns by their names (separated by a comma):
+
+-- SELECT column1, column2 
+-- FROM table_name;
+
+-- To make it easier to read, we moved FROM to another line.
+
+-- Line breaks don’t mean anything specific in SQL. We could write this entire query in one line, and it would run just fine.
+-- 1. Let’s only select the name and genre columns of the table.
+
+select name, genre
+from movies;
