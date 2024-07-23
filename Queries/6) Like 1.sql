1a3630538d749cc01a3a6161b18847db331389e8
mvsanghvi <mevsanghvi@gmail.com>
2024-07-23 17:48:53 -0500

    Redone

--- Queries/6) Like 1.sql
+++ Queries/6) Like 1.sql
@@ -1,7 +1,5 @@
 -- LIKE can be a useful operator when you want to compare similar values.
-
 -- The movies table contains two films with similar titles, ‘Se7en’ and ‘Seven’.
-
 -- How could we select all movies that start with ‘Se’ and end with ‘en’ and have exactly one character in the middle?
 
 -- SELECT * 
@@ -9,11 +7,8 @@
 -- WHERE name LIKE 'Se_en';
 
 --     LIKE is a special operator used with the WHERE clause to search for a specific pattern in a column.
-
 --     name LIKE 'Se_en' is a condition evaluating the name column for a specific pattern.
-
 --     Se_en represents a pattern with a wildcard character.
-
 -- The _ means you can substitute any individual character here without breaking the pattern. The names Seven and Se7en both match this pattern.
 select *
 from movies
