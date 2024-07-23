6f79510a0d0b6d453c24e7aa612d59c285e37d95
mvsanghvi <mevsanghvi@gmail.com>
2024-07-23 17:52:38 -0500

    Redone

--- Queries/12. Order By.sql
+++ Queries/12. Order By.sql
@@ -15,10 +15,12 @@
 -- *ASC is a keyword used in ORDER BY to sort the results in ascending order (low to high or A-Z).
 -- The column that we ORDER BY doesn’t even have to be one of the columns that we’re displaying. Note: ORDER BY always goes after WHERE (if WHERE is present).
 -- 1. Suppose we want to retrieve the name and year columns of all the movies, ordered by their name alphabetically. 
+
 select name, year
 from movies
 order by name;
 -- 2. Write a new query that retrieves the name, year, and imdb_rating columns of all the movies, ordered highest to lowest by their ratings.
+
 select name, year, imdb_rating
 from movies
 order by imdb_rating DESC;
