fcc22ae680e5dd42daeaaea86cddf47510b49583
mvsanghvi <mevsanghvi@gmail.com>
2024-07-26 18:55:10 -0500

    New Solution

--- Manipulation/Project 2.sql
+++ Manipulation/Project 2.sql
@@ -29,13 +29,13 @@ ORDER BY 2 DESC;
 -- 6. Hacker News stories are essentially links that take users to other websites. Which of these sites feed Hacker News the most:
 -- GitHub, Medium, or New York Times? First, we want to categorize each story based on their source. We can do this using a CASE statement:
 
-        -- SELECT CASE
-        -- WHEN url LIKE '%github.com%' THEN 'GitHub'
-        -- -- WHEN statement here
-        -- -- WHEN statement here
-        -- -- ELSE statement here
-        -- END AS 'Source'
-        -- FROM hacker_news;
+--         SELECT CASE
+--         WHEN url LIKE '%github.com%' THEN 'GitHub'
+--         -- WHEN statement here
+--         -- WHEN statement here
+--         -- ELSE statement here
+--         END AS 'Source'
+--         FROM hacker_news;
 
 -- Fill in the other WHEN statements and the ELSE statement.
 SELECT CASE
@@ -65,5 +65,34 @@ GROUP BY 1;
 
 -- Notice that the values are formatted like: 2018-05-08T12:30:00Z
 -- If you ignore the T and Z, the format is: YYYY-MM-DD HH:MM:SS
+-- 9. SQLite comes with a strftime() function - a very powerful function that allows you to return a formatted date. It takes two arguments:
+-- strftime(format, column) Let’s test this function out:
 
+--         SELECT timestamp,
+--         strftime('%H', timestamp)
+--         FROM hacker_news
+--         GROUP BY 1
+--         LIMIT 20;
+
+-- What do you think this does? Open the hint if you’d like to learn more.
+-- This returns the hour, HH, of the timestamp column! For strftime(__, timestamp):
+
+--     %Y returns the year (YYYY)
+--     %m returns the month (01-12)
+--     %d returns the day of the month (1-31)
+--     %H returns 24-hour clock (00-23)
+--     %M returns the minute (00-59)
+--     %S returns the seconds (00-59)
+
+-- if timestamp format is YYYY-MM-DD HH:MM:SS. Read more on the SQLite documentation.
+-- 10. Okay, now we understand how strftime() works. Let’s write a query that returns three columns:
+--     The hours of the timestamp
+--     The average score for each hour
+--     The count of stories for each hour
+SELECT strftime('%H', timestamp),
+   avg(score),
+   count(*)
+FROM hacker_news
+group by 1
+order by 2 desc;
 
