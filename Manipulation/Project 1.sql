5b7aca8d006cd8acc7476705da7a5f9d17f524fb
mvsanghvi <mevsanghvi@gmail.com>
2024-07-27 15:33:41 -0500

    More solutions

--- Manipulation/Project 1.sql
+++ Manipulation/Project 1.sql
@@ -8,5 +8,14 @@ FROM startups;
 -- 3. We want to know the total value of all companies in this table. Calculate this by getting the SUM() of the valuation column.
 SELECT SUM(valuation)
 FROM startups;
-
+-- 4. What is the highest amount raised by a startup? Return the maximum amount of money raised.
+SELECT MAX(raised)
+FROM startups;
+-- 5. Edit the query so that it returns the maximum amount of money raised, during ‘Seed’ stage
+SELECT MAX(raised)
+FROM startups
+WHERE stage = 'Seed';
+-- 6. In what year was the oldest company on the list founded?
+SELECT MIN(founded)
+FROM startups;
 
